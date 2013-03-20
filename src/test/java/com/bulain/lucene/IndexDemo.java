0e0d7ac9d8f7cbef71bcaa023e48de0778d7f882
bulain <bulain@gmail.com>
2013-03-20 11:02:58 +0800

    format code

--- src/test/java/com/bulain/lucene/IndexDemo.java
+++ src/test/java/com/bulain/lucene/IndexDemo.java
@@ -19,56 +19,56 @@ import org.junit.Test;
 
 public class IndexDemo {
 
-	@Test
-	public void testIndex() throws IOException {
-		String indexPath = "target/lucene";
-		boolean create = false;
+    @Test
+    public void testIndex() throws IOException {
+        String indexPath = "target/lucene";
+        boolean create = false;
 
-		Directory dir = FSDirectory.open(new File(indexPath));
-		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_32);
-		IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_32, analyzer);
+        Directory dir = FSDirectory.open(new File(indexPath));
+        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_32);
+        IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_32, analyzer);
 
-		if (create) {
-			iwc.setOpenMode(OpenMode.CREATE);
-		} else {
-			iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-		}
+        if (create) {
+            iwc.setOpenMode(OpenMode.CREATE);
+        } else {
+            iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+        }
 
-		IndexWriter writer = new IndexWriter(dir, iwc);
+        IndexWriter writer = new IndexWriter(dir, iwc);
 
-		String[][] fields = getFields();
-		for (int i = 0; i < fields.length; i++) {
-			String name = fields[i][0];
-			long length = i;
-			String contents = fields[i][1];
+        String[][] fields = getFields();
+        for (int i = 0; i < fields.length; i++) {
+            String name = fields[i][0];
+            long length = i;
+            String contents = fields[i][1];
 
-			Document doc = new Document();
+            Document doc = new Document();
 
-			Field nameField = new Field("name", name, Field.Store.YES, Field.Index.ANALYZED);
-			nameField.setOmitTermFreqAndPositions(true);
-			doc.add(nameField);
+            Field nameField = new Field("name", name, Field.Store.YES, Field.Index.ANALYZED);
+            nameField.setOmitTermFreqAndPositions(true);
+            doc.add(nameField);
 
-			NumericField modifiedField = new NumericField("modified", Field.Store.YES, false);
-			modifiedField.setLongValue(length);
-			doc.add(modifiedField);
+            NumericField modifiedField = new NumericField("modified", Field.Store.YES, false);
+            modifiedField.setLongValue(length);
+            doc.add(modifiedField);
 
-			doc.add(new Field("contents", contents, Field.Store.YES, Field.Index.NO));
+            doc.add(new Field("contents", contents, Field.Store.YES, Field.Index.NO));
 
-			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-				writer.addDocument(doc);
-			} else {
-				writer.updateDocument(new Term("name", name), doc);
-			}
+            if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+                writer.addDocument(doc);
+            } else {
+                writer.updateDocument(new Term("name", name), doc);
+            }
 
-		}
+        }
 
-		writer.close();
+        writer.close();
 
-	}
+    }
 
-	private String[][] getFields() {
-		return new String[][]{{"Test001", "This is a test 001."}, {"Test002", "This is a test 002."},
-		        {"Test003", "This is a test 003."}};
-	}
+    private String[][] getFields() {
+        return new String[][]{{"Test001", "This is a test 001."}, {"Test002", "This is a test 002."},
+                {"Test003", "This is a test 003."}};
+    }
 
 }
--- src/test/java/com/bulain/lucene/SearchDemo.java
+++ src/test/java/com/bulain/lucene/SearchDemo.java
@@ -18,28 +18,28 @@ import org.junit.Test;
 
 public class SearchDemo {
 
-	@Test
-	public void testSearch() throws IOException, ParseException {
-		String index = "target/lucene";
+    @Test
+    public void testSearch() throws IOException, ParseException {
+        String index = "target/lucene";
 
-		FSDirectory fs = FSDirectory.open(new File(index));
-		IndexSearcher searcher = new IndexSearcher(fs);
-		Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_32);
+        FSDirectory fs = FSDirectory.open(new File(index));
+        IndexSearcher searcher = new IndexSearcher(fs);
+        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_32);
 
-		QueryParser parser = new QueryParser(Version.LUCENE_32, "name", analyzer);
-		Query query = parser.parse("Test002");
-		
-		TopDocs topDocs = searcher.search(query, null, 100);
-		
-		for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-			Document doc = searcher.doc(scoreDoc.doc);
-			String name = doc.get("name");
-			String modified = doc.get("modified");
-			String contents = doc.get("contents");
-			
-			System.out.printf("%s %s %s\n", name, modified, contents);
-		}
-		
-	}
+        QueryParser parser = new QueryParser(Version.LUCENE_32, "name", analyzer);
+        Query query = parser.parse("Test*");
+
+        TopDocs topDocs = searcher.search(query, null, 100);
+
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            Document doc = searcher.doc(scoreDoc.doc);
+            String name = doc.get("name");
+            String modified = doc.get("modified");
+            String contents = doc.get("contents");
+
+            System.out.printf("%s %s %s\n", name, modified, contents);
+        }
+
+    }
 
 }
